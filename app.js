923be287827d81ed6c792f2204301a59e86d2671
Hannibal <88138693+HeyHannibal@users.noreply.github.com>
2022-04-24 02:44:22 +0300

    hid session secret in .env

--- app.js
+++ app.js
@@ -24,12 +24,15 @@ const actionsRouter = require("./routes/actions");
 const app = express();
 
 // passport
-app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+app.use(session({ secret: process.env.secret, resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
-// view engine setup
+// view engine setupapp.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 app.use(logger("dev"));
@@ -152,15 +155,99 @@ app.post(
 
 );
 
+
+
+
+
+
+
 app.get("/log-out", (req, res) => {
   req.logout();
   res.redirect("/");
 });
 
+app.get('/users', (req, res) => {
+  return res.send('GET HTTP method on user resource');
+});
+
+app.post('/users', (req, res) => {
+  return res.send('POST HTTP method on user resource');
+});
+
+app.put('/users/:userId', (req, res) => {
+  return res.send(
+    `PUT HTTP method on user/${req.params.userId} resource`,
+  );
+});
+
+app.delete('/users/:userId', (req, res) => {
+  return res.send(
+    `DELETE HTTP method on user/${req.params.userId} resource`,
+  );
+});
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
+
+
+
+
+let users = {
+  1: {
+    id: '1',
+    username: 'Robin Wieruch',
+  },
+  2: {
+    id: '2',
+    username: 'Dave Davids',
+  },
+};
+
+let messages = {
+  1: {
+    id: '1',
+    text: 'Hello World',
+    userId: '1',
+  },
+  2: {
+    id: '2',
+    text: 'Bye World',
+    userId: '2',
+  },
+};
+
+
+app.get('/users', (req, res) => {
+  return res.send(Object.values(users));
+});
+
+app.get('/users/:userId', (req, res) => {
+  return res.send(users[req.params.userId]);
+});
+
+app.get('/messages', (req, res) => {
+  return res.send(Object.values(messages));
+});
+
+app.get('/messages/:messageId', (req, res) => {
+  return res.send(messages[req.params.messageId]);
+});
+
+app.post('/messagez', (req, res) => {
+  const id = Math.random().toString();
+  const message = {
+    id,
+    text: req.body.text,
+
+  };
+
+  messages[id] = message;
+
+  return res.send(message);
+});
+
+
 
 // error handler
 app.use(function (err, req, res, next) {
